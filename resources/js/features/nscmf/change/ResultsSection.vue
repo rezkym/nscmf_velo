e5caa3034b1d9617411e3ed7ec0a2526df0598c1
Rezky Maulana <rezkymaulanaa06@gmail.com>
2026-09-20 14:54:56 +0700

    test: specify FE-26 behavior

---
+++ resources/js/features/nscmf/change/ResultsSection.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import type { ChangeDraftFields } from '../types';
+
+export type ResultFields = Pick<ChangeDraftFields, 'results'>;
+</script>
+
+<template>
+    <div></div>
+</template>
